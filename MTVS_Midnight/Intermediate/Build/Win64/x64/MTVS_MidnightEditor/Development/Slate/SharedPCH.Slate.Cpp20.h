ef0ba98a2227a4483ee142cfb7b75a605c7a8c9d
한승우 <tmddn_00@naver.com>
2024-08-20 19:36:34 +0900

    [Add] Create Project

---
+++ MTVS_Midnight/Intermediate/Build/Win64/x64/MTVS_MidnightEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Projects/Unreal/MTVS_Midnight_Unreal/MTVS_Midnight/Intermediate/Build/Win64/x64/MTVS_MidnightEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
---
+++ MTVS_Midnight/Source/MTVS_Midnight/MTVS_Midnight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MTVS_Midnight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MTVS_Midnight, "MTVS_Midnight" );
